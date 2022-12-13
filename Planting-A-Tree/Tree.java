6cf74acf77b7132a1275314896f98bd7f4235f22
mikeydonz <encarnan@oregonstate.edu>
2022-12-13 08:56:53 -0800

    Added Tree.java

---
+++ Planting-A-Tree/Tree.java
@@ -0,0 +1,15 @@
+public class Tree {
+  public static void main(String[] args) {
+    // this method will print a string to the console
+    System.out.println("Hey there!");
+    System.out.println("I'm going to plant a tree today!");
+    System.out.println();
+    System.out.println("  *** ");
+    System.out.println(" ***** ");
+    System.out.println("  *** ");
+    System.out.println("   * ");
+    System.out.println("   * ");
+    System.out.println("   * ");
+    System.out.println(" ##### ");
+  }
+}
